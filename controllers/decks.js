9c12f0f5e11a329c8c1aabd07586389df14ee082
Nachiappan14 <nicenach149@gmail.com>
2022-05-02 04:06:56 +0530

    bug fix in edit decks

--- controllers/decks.js
+++ controllers/decks.js
@@ -84,15 +84,19 @@ module.exports.editDeck = async function (req, res) {
     const { deckId, name, tags } = req.body;
 
     try {
-
         var deck = null;
 
         deckObjId = mongoose.Types.ObjectId(deckId);
         deck = await Deck.findById(deckObjId);
-        
+
         if (!deck) {
             flog(logText + "400");
             return res.status(400).json({ errors: [{ msg: "Deck Not Found" }] });
+        }
+
+        if (!deck.userId.includes(id)) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Not Authorized to modify deck" }] });
         }
 
         if (name) deck.name = name;
@@ -119,3 +123,60 @@ module.exports.editDeck = async function (req, res) {
         return res.status(500).send("Internal Server Error");
     }
 }
+
+/*
+module.exports.deleteDeck = async function (req, res) {
+    // logging the required informaton
+    var id = null;
+    if (req.user != undefined) id = req.user.id;
+    var logText = req.method + " " + req.originalUrl + " " + id + " ";
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        flog(logText + "400");
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { deckId } = req.body;
+
+    try {
+        let deck = await Deck.findById(deckId);
+        if (!deck) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Deck Not Found" }] });
+        }
+
+
+        var user = await User.findById(deck.userId);
+        if (!deck) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Deck Not Found" }] });
+        }
+
+        deck.cards = deck.cards.filter((ele) => (ele != cardId));
+
+        var operRes = await Card.deleteOne({ _id: cardId });
+
+        await deck.save();
+
+        // console.log(operRes);
+
+        if (operRes == null) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Card cannot be Deleted" }] });
+        }
+
+        if (operRes.modifiedCount < 1) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Card cannot be deleted" }] });
+        }
+        flog(logText + "200");
+        res.json({ msg: "Card Deletion Successful..." });
+
+    } catch (err) {
+        console.error("Error: ", err.message);
+        flog(logText + "500");
+        return res.status(500).send("Internal Server Error");
+    }
+}
+*/
