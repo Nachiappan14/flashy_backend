24e1420e81ebc0a9ebe2e55d1e859e5fd9403eb7
Nachiappan14 <nicenach149@gmail.com>
2022-05-02 00:40:25 +0530

    Added Editcard API and bug fix for addCard API

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,8 +29,8 @@ module.exports.addCard = async function (req, res) {
 
         // console.log(deck.userId, "==", id);
         // console.log(deck.userId.includes(id));
-        
-        if(!deck.userId.includes(id)){
+
+        if (!deck.userId.includes(id)) {
             flog(logText + "400");
             return res.status(400).json({ errors: [{ msg: "Not Authorized to add card" }] });
         }
@@ -47,7 +47,7 @@ module.exports.addCard = async function (req, res) {
         await card.save();
 
         // console.log(deck);
-        deck.cards.push(deck);
+        deck.cards.push(card);
 
         var operRes = await deck.save();
 
@@ -66,6 +66,92 @@ module.exports.addCard = async function (req, res) {
         }
         flog(logText + "200");
         res.json({ msg: "Card Addition Successful..." });
+
+    } catch (err) {
+        console.error("Error: ", err.message);
+        flog(logText + "500");
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+
+module.exports.editCard = async function (req, res) {
+    // logging the required informaton
+    var id = null;
+    if (req.user != undefined) id = req.user.id;
+    var logText = req.method + " " + req.originalUrl + " " + id + " ";
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        flog(logText + "400");
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { content, question, answer, deckId, cardId } = req.body;
+
+    try {
+        let oldCard = await Card.findById(cardId);
+        let card = await Card.findById(cardId);
+        if (!card) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Card Not Found" }] });
+        }
+
+        let oldDeck = await Deck.findById(card.deckId);
+
+        if (!oldDeck.userId.includes(id)) {
+            flog(logText + "400");
+            return res.status(400).json({ errors: [{ msg: "Not Authorized to access deck of card" }] });
+        }
+
+        var deck = null;
+        if (deckId) {
+            deck = await Deck.findById(deckId);
+            if (!deck) {
+                flog(logText + "400");
+                return res.status(400).json({ errors: [{ msg: "Deck Not Found" }] });
+            }
+            if (!deck.userId.includes(id)) {
+                flog(logText + "400");
+                return res.status(400).json({ errors: [{ msg: "Not Authorized to access Deck" }] });
+            }
+            card.deckId = deck;
+        }
+
+        oldDeck.cards = oldDeck.cards.filter((ele)=> (ele!=cardId));
+        await oldDeck.save();
+
+        if (content) card.content = content;
+        if (question) card.question = question;
+        if (answer) card.answer = answer;
+        // console.log(deck.userId, "==", id);
+        // console.log(deck.userId.includes(id));
+
+        await card.save();
+
+        deck.cards.push(card);
+
+        var operRes = await deck.save();
+
+        // console.log(operRes);
+
+        if (operRes == null) {
+            flog(logText + "400");
+            await Card.updateOne({ _id: card.id }, oldCard);
+            oldDeck.cards.push(oldCard);
+            await oldDeck.save();
+            return res.status(400).json({ errors: [{ msg: "Card cannot be Editted" }] });
+        }
+
+        if (operRes.modifiedCount < 1) {
+            flog(logText + "400");
+            await Card.updateOne({ _id: card.id }, oldCard);
+            oldDeck.cards.push(oldCard);
+            await oldDeck.save();
+            return res.status(400).json({ errors: [{ msg: "Card cannot be editted" }] });
+        }
+        flog(logText + "200");
+        res.json({ msg: "Card Edit Successful..." });
 
     } catch (err) {
         console.error("Error: ", err.message);
--- routes/cards.js
+++ routes/cards.js
@@ -4,16 +4,27 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 const { check } = require("express-validator");
 
-const {addCard} = require("../controllers/cards");
+const { addCard, editCard } = require("../controllers/cards");
 
-// Add Deck
+// Add Card
 router.post(
-    "/addCard",[
-        auth,
-        check("content","Enter Content for card").not().isEmpty(),
-        check("deckId","Deck Id not present").exists()
-    ],
+    "/addCard", [
+    auth,
+    check("content", "Enter Content for card").not().isEmpty(),
+    check("deckId", "Deck Id not present").exists()
+],
     addCard
 );
+
+
+// Edit Card
+router.post(
+    "/editCard", [
+    auth,
+    check("cardId", "Card Id does not exist").exists()
+],
+    editCard
+);
+
 
 module.exports = router;
